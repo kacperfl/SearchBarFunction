c5e09ddf5e496c5ef3cec179af3a69af22ce2b19
kacperfl <134588250+kacperfl@users.noreply.github.com>
2024-02-14 10:00:53 +0100

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+// App.jsx
+import React, { useState } from 'react';
+import './App.css';
+import { SearchBar } from './components/SearchBar';
+import { SearchResultsList } from './components/SearchResultsList';
+
+
+function App() {
+  const [results, setResults] = useState([]);
+
+  return (
+    <div className='App'>
+      <div className='search-bar-container'>
+        {/* Zoekbalk en zoekresultaten  */}
+        <SearchBar setResults={setResults} />
+        <SearchResultsList results={results} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
